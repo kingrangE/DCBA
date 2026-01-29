3b3b5c72895d4148fe04c3f5422895b90d1932ee
kingrangE <kilwon.jeon00@gmail.com>
2026-01-29 18:10:42 +0900

    init : Initialize DCBA JAVA Project
    Spring Boot 기반의 JAVA Project 생성

---
+++ DCBA/src/main/java/kingrangE/DCBA/DcbaApplication.java
@@ -0,0 +1,13 @@
+package kingrangE.DCBA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcbaApplication.class, args);
+	}
+
+}
---
+++ DCBA/src/test/java/kingrangE/DCBA/DcbaApplicationTests.java
@@ -0,0 +1,13 @@
+package kingrangE.DCBA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
