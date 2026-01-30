e940e33008234107e6ca6d03d9d3b3911ad1e749
kingrangE <kilwon.jeon00@gmail.com>
2026-01-30 13:49:33 +0900

    refactor : User Id를 이용하여 문제 추가 및 차단이 가능하게 수정

--- DCBA/src/main/java/kingrangE/DCBA/repository/UserRepository.java
+++ DCBA/src/main/java/kingrangE/DCBA/repository/UserRepository.java
@@ -11,6 +11,6 @@ public interface UserRepository {
     Optional<User> findById(String id); // 유저 ID를 받아 User 정보 반환
     Optional<User> findByName(String name); // 유저 Name을 받아 User 정보 반환
     User deleteById(String id); // 유저 ID를 받아 User 정보 삭제 후, 삭제된 유저 정보 반환
-    void addExerciseNumber(Integer exerciseNumber); // 문제 번호를 전달받아 유저가 받은 문제 리스트에 추가
-    void addExerciseBlackList(Integer exerciseNumber); // Exercise BlackList에 번호 추가
+    void addExerciseNumber(String id,Integer exerciseNumber); // 문제 번호를 전달받아 유저가 받은 문제 리스트에 추가
+    void addExerciseBlackList(String id,Integer exerciseNumber); // Exercise BlackList에 번호 추가
 }
