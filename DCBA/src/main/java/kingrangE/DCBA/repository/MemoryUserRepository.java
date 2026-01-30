ef44464a8b3d1bb0259e7e47742053eb04a716e9
kingrangE <kilwon.jeon00@gmail.com>
2026-01-30 14:41:09 +0900

    feat : HashMap을 이용한 임시 User DB 구현체 제작

---
+++ DCBA/src/main/java/kingrangE/DCBA/repository/MemoryUserRepository.java
@@ -0,0 +1,44 @@
+package kingrangE.DCBA.repository;
+
+import kingrangE.DCBA.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryUserRepository implements UserRepository{
+    private ConcurrentHashMap<String,User> repo = new ConcurrentHashMap<>();
+
+    @Override
+    public String save(User user) {
+        repo.put(user.getId(),user);
+        return user.getId();
+    }
+
+    @Override
+    public Optional<User> findById(String id) {
+        return Optional.ofNullable(repo.get(id));
+    }
+
+    @Override
+    public Optional<User> findByName(String name) {
+        return repo.values().stream().filter(user -> user.getName().equals(name)).findAny();
+    }
+
+    @Override
+    public User deleteById(String id) {
+        return repo.remove(id);
+    }
+
+    @Override
+    public void addExerciseNumber(String id,Integer exerciseNumber) {
+        User user = repo.get(id);
+        user.getExercises().add(exerciseNumber);
+    }
+
+    @Override
+    public void addExerciseBlackList(String id,Integer exerciseNumber) {
+        User user = repo.get(id);
+        user.getBlacklist().add(exerciseNumber);
+    }
+}
