a76dd85ba56191d11bb80ba0d14f7abe7472f9ef
kingrangE <kilwon.jeon00@gmail.com>
2026-01-30 18:04:31 +0900

    feat : Add constructor for initializing model
    User 회원가입에 이용하기 위한 생성자 생성
    - 수준 모두 1로 시작(Map초기화)
    - 받은 문제 및 차단 문제 : 빈 리스트
    - 그 외는 값을 전달받아 초기화

--- DCBA/src/main/java/kingrangE/DCBA/domain/User.java
+++ DCBA/src/main/java/kingrangE/DCBA/domain/User.java
@@ -1,5 +1,7 @@
 package kingrangE.DCBA.domain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -8,9 +10,27 @@ public class User {
     private String name; // 로그인을 위해 사용할 이름
     private String password; // 나중에 보안 되도록 변경해야 한다. (로그인해서 할 수 있는게 조회뿐이므로 일단은 그냥 생으로 저장)
     private List<String> mainLanguages; // User Main Language;
-    private List<Map<String,Integer>> subjectLevels; // User의 각 과목 Level
+    private Map<String,Integer> subjectLevels; // User의 각 과목 Level
     private List<Integer> exercises; // User가 지금까지 받은 문제 번호 목록
     private List<Integer> blacklist;
+
+    public User(String id, String name, String password, List<String> mainLanguages) {
+        // 기본 회원가입 ( 과목 난이도 설정 불가, 문제 받은목록, 차단목록 초기화)
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.mainLanguages = mainLanguages;
+        this.subjectLevels = new HashMap<>(Map.of(
+                Subject.DATA_STRUCTURE.getSubjectName(),1,
+                Subject.ALGORITHM.getSubjectName(),1,
+                Subject.COMPUTER_ARCHITECTURE.getSubjectName(),1,
+                Subject.OPERATING_SYSTEM.getSubjectName(),1,
+                Subject.DATABASE.getSubjectName(),1,
+                Subject.COMPUTER_NETWORK.getSubjectName(),1
+        ));
+        this.exercises = new ArrayList<>();
+        this.blacklist = new ArrayList<>();
+    }
 
     public String getName() {
         return name;
@@ -28,11 +48,11 @@ public class User {
         this.password = password;
     }
 
-    public List<Map<String, Integer>> getSubjectLevels() {
+    public Map<String, Integer> getSubjectLevels() {
         return subjectLevels;
     }
 
-    public void setSubjectLevels(List<Map<String, Integer>> subjectLevels) {
+    public void setSubjectLevels(Map<String, Integer> subjectLevels) {
         this.subjectLevels = subjectLevels;
     }
 
