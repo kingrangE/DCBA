085c6e37d6d8319be6a818d94cbc8446c3fff8f8
kingrangE <kilwon.jeon00@gmail.com>
2026-02-03 20:00:03 +0900

    test : UserService Unit Test 코드 작성
    - 특징
    	1. DI로 구현하지 않고, 직접 넣음. -> Unit Test 효율성을 위해 Exercise Repo처럼 함
    	2. Error 검증을 위해 Assertions의 assertThatThrownBy를 사용
    - 테스트 내용
    	1. 회원가입 성공 / ID 중복 실패 / 닉네임 중복 실패
    	2. 로그인 성공 / 없는 닉네임 실패 / 틀린 비밀번호 실패

---
+++ DCBA/src/test/java/kingrangE/DCBA/service/UserServiceTest.java
@@ -0,0 +1,92 @@
+package kingrangE.DCBA.service;
+
+import kingrangE.DCBA.domain.User;
+import kingrangE.DCBA.repository.MemoryUserRepository;
+import kingrangE.DCBA.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class UserServiceTest {
+    UserService userService;
+    UserRepository userRepository;
+
+    @BeforeEach
+    void setup() {
+        userRepository = new MemoryUserRepository();
+        userService = new UserService(userRepository);
+    }
+
+    @Test
+    @DisplayName("회원가입 - 성공")
+    void signUpSuccess() {
+        // given
+        // when
+        userService.signUp("1", "길원", "1", List.of("x"));
+        // then
+        User findedUser = userRepository.findById("1").orElse(null); // 조회
+        Assertions.assertThat(findedUser).isNotNull(); // 있으면 성공
+    }
+
+    @Test
+    @DisplayName("회원가입 - ID 중복 실패")
+    void signUpIdDuplicated() {
+        // given
+        userService.signUp("1", "길원", "1", List.of("x"));
+        // when
+        // then
+        Assertions.assertThatThrownBy(() -> userService.signUp("1", "길원1", "1", List.of("x")))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("해당 ID로 가입한 유저가 존재합니다.");
+    }
+
+    @Test
+    @DisplayName("회원가입 - 닉네임 중복 실패")
+    void signUpNameDuplicated() {
+        // given
+        userService.signUp("1", "길원", "1", List.of("x"));
+        // when
+        // then
+        Assertions.assertThatThrownBy(() -> userService.signUp("2", "길원", "1", List.of("x")))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("해당 닉네임을 사용하는 유저가 존재합니다.");
+    }
+
+    @Test
+    @DisplayName("로그인 - 성공")
+    void loginSuccess() {
+        // given
+        userService.signUp("1", "길원", "1", List.of("x"));
+        // when
+        User loginnedUser = userService.login("길원", "1");
+        // then
+        Assertions.assertThat(loginnedUser).isNotNull(); // 있으면 성공
+    }
+
+    @Test
+    @DisplayName("로그인 - 존재하지 않는 닉네임")
+    void loginNoSuchName() {
+        // given
+        userService.signUp("1", "길원", "1", List.of("x"));
+        // when
+        // then
+        Assertions.assertThatThrownBy(() -> userService.login("길1", "1"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("존재하지 않는 아이디입니다.");
+    }
+
+    @Test
+    @DisplayName("로그인 - 비밀번호 틀림")
+    void loginWrongPassword() {
+        // given
+        userService.signUp("1", "길원", "1", List.of("x"));
+        // when
+        // then
+        Assertions.assertThatThrownBy(() -> userService.login("길원", "2"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("비밀번호가 틀립니다.");
+    }
+}
