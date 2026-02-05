78a019d44cdccbd1a190fbeb7b99736fac7bf0d5
kingrangE <kilwon.jeon00@gmail.com>
2026-02-05 09:37:14 +0900

    refactor : lombok을 이용하여 코드 간소화(Setter 제거함)
    - Entity의 무결성을 지키기 위해 Setter를 제거했습니다.
    	- 나도 모르게 set으로 설정할 수도 있기 때문

--- DCBA/src/main/java/kingrangE/DCBA/domain/Exercise.java
+++ DCBA/src/main/java/kingrangE/DCBA/domain/Exercise.java
@@ -1,58 +1,15 @@
 package kingrangE.DCBA.domain;
 
+import lombok.*;
+
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor
 public class Exercise {
     private Integer num;
-    private String title;
-    private String best_answer;
+    private String question;
+    private String answer;
     private Subject subject;
     private Level level;
-
-    public Exercise(Integer num, String title, String best_answer, Subject subject, Level level) {
-        this.num = num;
-        this.title = title;
-        this.best_answer = best_answer;
-        this.subject = subject;
-        this.level = level;
-    }
-
-    public Integer getNum() {
-        return num;
-    }
-
-    public void setNum(Integer num) {
-        this.num = num;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBest_answer() {
-        return best_answer;
-    }
-
-    public void setBest_answer(String best_answer) {
-        this.best_answer = best_answer;
-    }
-
-
-    public Subject getSubject() {
-        return subject;
-    }
-
-    public void setSubject(Subject subject) {
-        this.subject = subject;
-    }
-
-    public Level getLevel() {
-        return level;
-    }
-
-    public void setLevel(Level level) {
-        this.level = level;
-    }
 }
--- DCBA/src/main/java/kingrangE/DCBA/domain/User.java
+++ DCBA/src/main/java/kingrangE/DCBA/domain/User.java
@@ -1,90 +1,32 @@
 package kingrangE.DCBA.domain;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@AllArgsConstructor
 public class User {
     private String id; // User Slack ID;
     private String name; // 로그인을 위해 사용할 이름
     private String password; // 나중에 보안 되도록 변경해야 한다. (로그인해서 할 수 있는게 조회뿐이므로 일단은 그냥 생으로 저장)
     private List<String> mainLanguages; // User Main Language;
-    private Map<String,Integer> subjectLevels; // User의 각 과목 Level
     private List<Integer> exercises; // User가 지금까지 받은 문제 번호 목록
     private List<Integer> blacklist;
 
+    @Builder
     public User(String id, String name, String password, List<String> mainLanguages) {
         // 기본 회원가입 ( 과목 난이도 설정 불가, 문제 받은목록, 차단목록 초기화)
         this.id = id;
         this.name = name;
         this.password = password;
         this.mainLanguages = mainLanguages;
-        this.subjectLevels = new HashMap<>(Map.of(
-                Subject.DATA_STRUCTURE.getSubjectName(),1,
-                Subject.ALGORITHM.getSubjectName(),1,
-                Subject.COMPUTER_ARCHITECTURE.getSubjectName(),1,
-                Subject.OPERATING_SYSTEM.getSubjectName(),1,
-                Subject.DATABASE.getSubjectName(),1,
-                Subject.COMPUTER_NETWORK.getSubjectName(),1
-        ));
         this.exercises = new ArrayList<>();
         this.blacklist = new ArrayList<>();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Map<String, Integer> getSubjectLevels() {
-        return subjectLevels;
-    }
-
-    public void setSubjectLevels(Map<String, Integer> subjectLevels) {
-        this.subjectLevels = subjectLevels;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public List<String> getMainLanguages() {
-        return mainLanguages;
-    }
-
-    public void setMainLanguages(List<String> mainLanguage) {
-        this.mainLanguages = mainLanguage;
-    }
-
-    public List<Integer> getExercises() {
-        return exercises;
-    }
-
-    public void setExercises(List<Integer> exercises) {
-        this.exercises = exercises;
-    }
-
-    public List<Integer> getBlacklist() {
-        return blacklist;
-    }
-
-    public void setBlacklist(List<Integer> blacklist) {
-        this.blacklist = blacklist;
     }
 }
