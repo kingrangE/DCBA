3565cab69b2086abb71dc5c4297ce1a6e752c83f
kingrangE <kilwon.jeon00@gmail.com>
2026-02-06 14:01:14 +0900

    feat: User view controller와 UI 구현
    1. 회원가입
    2. 로그인

--- DCBA/src/main/java/kingrangE/DCBA/controller/UserController.java
+++ DCBA/src/main/java/kingrangE/DCBA/controller/UserController.java
@@ -7,52 +7,84 @@ import kingrangE.DCBA.dto.LoginForm;
 import kingrangE.DCBA.dto.SignUpForm;
 import kingrangE.DCBA.repository.UserRepository;
 import kingrangE.DCBA.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
-    @Autowired
-    public UserController(UserRepository userRepo, UserService userService) {
-        this.userService = userService;
-    }
 
+    /**
+     * URL 접속 시 메인 페이지 (Login)
+     * @param model HTML 파일에 전달할 모델 객체
+     * @return login.html
+     */
     @GetMapping("/")
     public String loginForm(Model model) {
-        model.addAttribute("loginForm", new LoginForm()); // 빈 객체 전달
+        // 로그인을 위한 LoginForm
+        model.addAttribute("loginForm", new LoginForm());
         return "login";
     }
 
+    /**
+     * login 요청 시 로직
+     * @param loginForm LoginFrom DTO
+     * @param session Session (login 결과 저장)
+     * @return dashboard(redirect)
+     */
     @PostMapping("/login")
-    public String login(@ModelAttribute LoginForm loginForm, HttpSession session){
+    public String login(@ModelAttribute LoginForm loginForm, HttpSession session) {
+
+        // 이름과 비밀번호로 로그인 진행
         User user = userService.login(loginForm.getName(), loginForm.getPassword());
+
+        // User Login Session 유지를 위한 Attribute 추가
         session.setAttribute("loginUser", user);
+
         return "redirect:/dashboard";
     }
+
+    /**
+     * 회원가입 화면 요청 시 로직
+     * @param model HTML 파일에 전달할 모델 객체
+     * @return signup.html
+     */
     @GetMapping("/signup")
-    public String signupForm(Model model){
-        model.addAttribute("signUpForm",new SignUpForm());
+    public String signupForm(Model model) {
+        // 회원가입을 위한 회원가입 Form 전달
+        model.addAttribute("signUpForm", new SignUpForm());
         return "signup";
     }
+
+    /**
+     * 회원가입 요청 시, 처리 로직
+     * @param signUpForm 회원가입 형식 DTO
+     * @return login(redirect)
+     */
     @PostMapping("/signup")
     public String signup(@ModelAttribute SignUpForm signUpForm) {
-        userService.signUp(signUpForm.getId(),
-                signUpForm.getName(),
-                signUpForm.getPassword(),
-                signUpForm.getMainLanguages());
+        userService.signUp(signUpForm.getName(),
+                signUpForm.getPassword());
         return "redirect:/";
     }
 
-    @ExceptionHandler({RuntimeException.class})
-    public String handlerException(RuntimeException e, HttpServletRequest request, RedirectAttributes redirectAttributes){
+
+    /**
+     * UserController에서 발생한 모든 종류의 Runtime Error 발생 시, 처리하기 위한 Handler
+     * @param e Error
+     * @param request Error가 발생한 HTTP 요청
+     * @param redirectAttributes 데이터 전달하기 위한 객체 (Error Message를 전달하기 위함)
+     * @return
+     */
+    @ExceptionHandler({ RuntimeException.class })
+    public String handlerException(RuntimeException e, HttpServletRequest request,
+            RedirectAttributes redirectAttributes) {
         // Error 메시지를 같이 보내주기 위함
         redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
 
