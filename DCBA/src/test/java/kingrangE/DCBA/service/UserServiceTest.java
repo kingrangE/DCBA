40238b73b6c393606de434dfd7cb73cfec69007f
kingrangE <kilwon.jeon00@gmail.com>
2026-02-09 17:46:27 +0900

    refactor: H2 DB->MariaDB

--- DCBA/src/test/java/kingrangE/DCBA/service/UserServiceTest.java
+++ DCBA/src/test/java/kingrangE/DCBA/service/UserServiceTest.java
@@ -8,10 +8,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("local")
 public class UserServiceTest {
     UserService userService;
     @Autowired
