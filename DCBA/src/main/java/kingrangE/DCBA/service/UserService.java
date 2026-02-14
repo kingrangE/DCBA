758a3354242d5fb109b892fdf2cc3b18983bf246
kingrangE <kilwon.jeon00@gmail.com>
2026-02-14 14:59:16 +0900

    fix: Not을 추가하여 틀렸을 때 맞았다고 하는 오류 해결

--- DCBA/src/main/java/kingrangE/DCBA/service/UserService.java
+++ DCBA/src/main/java/kingrangE/DCBA/service/UserService.java
@@ -15,16 +15,17 @@ public class UserService {
 
     /**
      * 회원가입 로직
-     * @param name 이름
+     * 
+     * @param name     이름
      * @param password 비번
      */
-    public void signUp(String name, String password){
+    public void signUp(String name, String password) {
         // 이미 사용 중인 경우 Error
-        if (userRepo.findByName(name).orElse(null)!=null){
+        if (userRepo.findByName(name).orElse(null) != null) {
             throw new RuntimeException("해당 닉네임을 사용하는 유저가 존재합니다.");
         }
         // 암호화
-        String encoded_password = BCrypt.hashpw(password,BCrypt.gensalt());
+        String encoded_password = BCrypt.hashpw(password, BCrypt.gensalt());
         // 암호화된 비밀번호로 로그인
         User user = new User(name, encoded_password);
         userRepo.save(user);
@@ -32,18 +33,19 @@ public class UserService {
 
     /**
      * 로그인 로직
-     * @param name 이름
+     * 
+     * @param name     이름
      * @param password 비번
      * @return User 객체
      */
-    public User login(String name, String password){
+    public User login(String name, String password) {
         User user = userRepo.findByName(name).orElse(null);
 
-        if (user == null){
+        if (user == null) {
             throw new RuntimeException("존재하지 않는 아이디입니다.");
         }
-        String userPassword= user.getPassword();
-        if (BCrypt.checkpw(password,userPassword)){
+        String userPassword = user.getPassword();
+        if (!BCrypt.checkpw(password, userPassword)) {
             throw new RuntimeException("비밀번호가 틀립니다.");
         }
         return user;
